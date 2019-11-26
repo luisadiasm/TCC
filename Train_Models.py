3285d36301114a41521b87192513b7df2d4c259d
luisadiasm <38387858+luisadiasm@users.noreply.github.com>
2019-11-25 23:57:54 -0300

    Add files via upload

--- Train_Models.py
+++ Train_Models.py
@@ -3,9 +3,10 @@ import sklearn
 from sklearn import svm, preprocessing, naive_bayes,neighbors, neural_network, tree
 import numpy as np
 from joblib import dump, load
+from sklearn.neural_network import MLPRegressor
 
-df = pd.read_csv('dataframe.csv', index_col=[0],sep=';') 
-
+df = pd.read_csv('dataframe.csv',sep=';') 
+df['Preco_arq'] = df['Preco_arq']/10000
 df = df.fillna(0)
 #df.drop(df.tail(1).index,inplace=True)
 df = sklearn.utils.shuffle(df)
@@ -13,9 +14,10 @@ df = sklearn.utils.shuffle(df)
 del df['NomeArquivo']
 
 x = df.drop('Preco_arq',axis=1).values
-y = df['Preco_arq'].values
-
-test_size = 50
+#y = df['Preco_arq'].values
+#y = np.asarray(df['Preco_arq'], dtype="|S6")
+y = list(df['Preco_arq'])
+test_size = 70
 
 x_train = x[:-test_size]
 y_train = y[:-test_size]
@@ -23,30 +25,12 @@ y_train = y[:-test_size]
 x_test = x[-test_size:]
 y_test = y[-test_size:]
 
-
 print('---MLP---')
-clf = neural_network.MLPClassifier(hidden_layer_sizes=(100,100,50,50))
+#clf = neural_network.MLPClassifier(hidden_layer_sizes=(10,10,5,5),max_iter=5000)
+clf = MLPRegressor(hidden_layer_sizes=(100,200), max_iter=500)
 clf.fit(x_train,y_train)
 print(clf.score(x_test,y_test))
 dump(clf, 'MLP.pkl', compress=9)
-
-print('---Naive Bayers---')
-clf = naive_bayes.BernoulliNB()
-clf.fit(x_train,y_train)
-print(clf.score(x_test,y_test))
-dump(clf, 'NVB.pkl', compress=9)
-
-print('---Nearest Neightbors---')
-clf = neighbors.KNeighborsClassifier()
-clf.fit(x_train,y_train)
-print(clf.score(x_test,y_test))
-dump(clf, 'NN.pkl', compress=9)
-
-print('---Decision Tree---')
-clf = tree.DecisionTreeClassifier()
-clf.fit(x_train,y_train)
-print(clf.score(x_test,y_test))
-dump(clf, 'DT.pkl', compress=9)
 
 #for x,y in zip(x_test,y_test):
 #    print(f"Model:{clf4.predict([x])[0]}, Actual: {y}")
